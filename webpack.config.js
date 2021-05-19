f635f980aed614b2d205d2a4b289eb0de173c7c5
lee <1079811936@qq.com>
2021-05-20 02:26:59 +0800

    updata

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+      },
     entry: './src/index.js',//输入路径，默认src/index.js
     output: {
       filename: '[name].[contenthash].js',//输出的文件名，可改
